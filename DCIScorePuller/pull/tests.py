510d0e55d6222438ca095e59f4ee3ddc7e54c4ca
Ethan Christensen <ethanlchristensen@outlook.com>
2023-07-30 01:18:52 -0500

    test

--- DCIScorePuller/pull/tests.py
+++ DCIScorePuller/pull/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from pull.models import Corp
 
 # Create your tests here.
+class CorpTestCase(TestCase):
+    def setUp(self):
+        Corp.objects.create(key=69, name="ATTT Corp")
+    
+    def test_corp_name(self):
+        test_corp = Corp.objects.get(key=69)
+        self.assertEquals(test_corp.name, "ATTT Corp")
