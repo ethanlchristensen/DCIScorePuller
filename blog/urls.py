c1397e8e7966c0cf0aa2fca12a5bdda539db959c
Ethan Christensen <ethanlchristensen@outlook.com>
2023-08-05 19:13:02 -0500

    create and update post done'

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,9 @@ from .views import *
 
 urlpatterns = [
     path("blog/", view=home, name="blog-home"),
-    path("blog/like/<int:post_id>", view=like_unlike_post, name="like-post"),
-    path("blog/unlike/<int:post_id>", view=like_unlike_post, name="unlike-post"),
+    path("blog/post/like/<int:post_id>", view=like_unlike_post, name="like-post"),
+    path("blog/post/unlike/<int:post_id>", view=like_unlike_post, name="unlike-post"),
+    path("blog/post/create", view=create_post, name="create-post"),
+    path("blog/post/delete/<int:post_id>", view=delete_post, name="delete-post"),
+    path("blog/post/update/<int:post_id>", view=update_post, name="update-post"),
 ]
